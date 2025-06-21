fb49e711ef96632134737365043a765ab202fff5
ambupada <muduliambupada.com>
2025-06-21 20:36:03 +0530

    exploring get,post,delete call

--- src/app.js
+++ src/app.js
@@ -2,21 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req, res) => {
-    res.send("Hello welcome");
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Ambupada" , lastName: "Muduli" });
 });
 
-app.use("/lol",(req, res) => {
+//
+app.post("/user", (req, res) => {
+    res.send("Data successfully send to data bases");
+})
+
+//
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully");
+})
+
+//This will match all the HTTP method API calls to /hello
+app.use("/hello",(req, res) => {
     res.send("Hello welcome to dashboard");
 });
 
-app.use("/test",(req, res) => {
-    res.send("Hello from test");
-});
 
-app.use("/server", (req, res) => {
-    res.send("Hello from serverrr");
-});
 
 app.listen(7777 , () => {
     console.log("Server is up and running on port 7777");
