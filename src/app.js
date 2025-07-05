b4c76e0db7d543d636e63fb2b8eb79ae0cdc23d9
ambupada <muduliambupada.com>
2025-07-05 15:47:38 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,33 +2,31 @@ const express = require("express");
 
 const app = express();
 
-//This will only handle GET call to /user
-app.use("/user", (req , res) => {
-  res.send({ firstName: "Ambupada" , lastName: "Muduli" });
-});
+//app.use("/route", rH, rH2, rH3, rH4);
 
-//This will only handle GET call to /user
-// app.get("/user/:userId/:name/:password", (req, res) => {
-//     console.log(req.params);
-//     res.send({ firstName: "Ambupada" , lastName: "Muduli" });
-// });
-
-app.post("/user", (req , res) => {
-  console.log(req.body);
-  //saving data to DB
-  res.send("Data successfully saved to DB");
-});
-
-app.delete("/user", (req , res) => {
-  res.send("Deleted successfully");
-});
-
-//this will match the all the HTTP method API calls to /test
-app.use("/test", (req , res) => {
-  res.send("Hello from server");
-});
-
-
+app.get("/user", 
+  (req , res , next) => {
+  //Route Handler
+  //res.send("Hello");
+  next();
+},
+(req ,res, next) => {
+  res.send("Welcome 1");
+  next();
+},
+(req ,res, next) => {
+  res.send("Welcome 2");
+  next();
+},
+(req ,res, next) => {
+  res.send("Welcome 3");
+  next();
+},
+(req ,res, next) => {
+  res.send("Welcome 4");
+  next();
+},
+);
 
 app.listen(7777 , () => {
     console.log("Server is up and running on port 7777");
