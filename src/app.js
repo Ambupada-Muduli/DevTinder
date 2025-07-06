770bc7d1a34e1ebef8473c3b2aa26524f047f021
ambupada <muduliambupada.com>
2025-07-06 12:00:06 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -5,28 +5,60 @@ const User = require("./models/user");
 
 app.use(express.json());
 
-app.post("/signup", async (req , res) => {
+app.post("/signup", async (req, res) => {
 
-    //Creating a new instance of the User model
-  const user = new User (req.body);
+  //Creating a new instance of the User model
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added successfully");
-  } catch(err) {
-    res.status(400).send("Error saving the user" +err.message)
+  } catch (err) {
+    res.status(400).send("Error saving the user" + err.message)
   }
 
 })
 
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail});
+    //res.send(user);
+    if(!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  //   const user = await User.find({ emailId: userEmail });
+  //   if(user.length === 0) {
+  //   res.status(404).send("User not found");
+  // } else {
+  //   res.send(user);
+  // }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Feed API - GET/Feed - get all the users from database
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.send(user);
+  } catch(err) {
+    res.status(400).send("User is not found");
+  }
+});
 
 connectDB()
-    .then( () => {
-        console.log("database connection is established");
-        app.listen(7777 , () => {
-    console.log("Server is up and running on port 7777");
-});
-    })
-    .catch((err) => {
-        console.err("Database can not be connected");
+  .then(() => {
+    console.log("database connection is established");
+    app.listen(7777, () => {
+      console.log("Server is up and running on port 7777");
     });
+  })
+  .catch((err) => {
+    console.err("Database can not be connected");
+  });
